24aa56357471f34aaf8f4d82f899692d4f93c7f1
lunfardo314 <evaldas.drasutis@iota.org>
2022-08-21 10:59:39 +0300

    slicearray encoding

--- slicearray_test.go
+++ slicearray_test.go
@@ -2,6 +2,7 @@ package easyutxo
 
 import (
 	"bytes"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,11 +12,11 @@ var data = []byte("a")
 
 func TestParams(t *testing.T) {
 	t.Run("1", func(t *testing.T) {
-		da := NewParams(nil)
+		da := SliceArrayFromBytes(nil)
 		da.Push(data)
 		require.EqualValues(t, 1, da.NumElements())
 		ser := da.Bytes()
-		daBack := NewParams(ser)
+		daBack := SliceArrayFromBytes(ser)
 		require.EqualValues(t, 1, daBack.NumElements())
 		require.EqualValues(t, da.At(0), daBack.At(0))
 		require.Panics(t, func() {
@@ -26,11 +27,11 @@ func TestParams(t *testing.T) {
 		})
 	})
 	t.Run("2", func(t *testing.T) {
-		da := NewParams(nil)
+		da := SliceArrayFromBytes(nil)
 		da.Push(data)
 		require.EqualValues(t, 1, da.NumElements())
 		serFull := da.Bytes()
-		daBack := NewParams(serFull)
+		daBack := SliceArrayFromBytes(serFull)
 		require.EqualValues(t, 1, daBack.NumElements())
 		require.EqualValues(t, da.At(0), daBack.At(0))
 		require.Panics(t, func() {
@@ -41,7 +42,7 @@ func TestParams(t *testing.T) {
 		})
 	})
 	t.Run("not empty", func(t *testing.T) {
-		da := NewParams(nil)
+		da := SliceArrayFromBytes(nil)
 		daBytes := da.Bytes()
 		require.EqualValues(t, []byte{0, 0}, daBytes)
 		require.NotPanics(t, func() {
@@ -53,46 +54,64 @@ func TestParams(t *testing.T) {
 	})
 	t.Run("too long", func(t *testing.T) {
 		require.NotPanics(t, func() {
-			da := NewParams(nil)
+			da := SliceArrayFromBytes(nil)
 			da.Push(bytes.Repeat(data, 256))
 		})
 		require.NotPanics(t, func() {
-			da := NewParams(nil)
+			da := SliceArrayFromBytes(nil)
 			da.Push(bytes.Repeat(data, 257))
 		})
 		require.NotPanics(t, func() {
-			da := NewParams(nil)
+			da := SliceArrayFromBytes(nil)
 			for i := 0; i < 256; i++ {
 				da.Push(data)
 			}
 		})
-		require.Panics(t, func() {
-			da := NewParams(nil)
+		require.NotPanics(t, func() {
+			da := SliceArrayFromBytes(nil)
 			for i := 0; i < 257; i++ {
+				da.Push(data)
+			}
+		})
+		require.Panics(t, func() {
+			da := SliceArrayFromBytes(nil)
+			for i := 0; i < math.MaxUint16+1; i++ {
 				da.Push(data)
 			}
 		})
 	})
 	t.Run("serialization short", func(t *testing.T) {
-		da := NewParams(nil)
+		da := SliceArrayFromBytes(nil)
 		for i := 0; i < 100; i++ {
 			da.Push(bytes.Repeat(data, 100))
 		}
-		daBack := NewParams(da.Bytes())
+		daBack := SliceArrayFromBytes(da.Bytes())
 		require.EqualValues(t, da.NumElements(), daBack.NumElements())
-		for i := byte(0); i < 100; i++ {
+		for i := 0; i < 100; i++ {
 			require.EqualValues(t, da.At(i), daBack.At(i))
 		}
 	})
-	t.Run("serialization long", func(t *testing.T) {
-		da := NewParams(nil)
+	t.Run("serialization long 1", func(t *testing.T) {
+		da := SliceArrayFromBytes(nil)
 		for i := 0; i < 100; i++ {
 			da.Push(bytes.Repeat(data, 2000))
 		}
 		daBytes := da.Bytes()
-		daBack := NewParams(daBytes)
+		daBack := SliceArrayFromBytes(daBytes)
 		require.EqualValues(t, da.NumElements(), daBack.NumElements())
-		for i := byte(0); i < 100; i++ {
+		for i := 0; i < 100; i++ {
+			require.EqualValues(t, da.At(i), daBack.At(i))
+		}
+	})
+	t.Run("serialization long 2", func(t *testing.T) {
+		da := SliceArrayFromBytes(nil)
+		for i := 0; i < 500; i++ {
+			da.Push(bytes.Repeat(data, 2000))
+		}
+		daBytes := da.Bytes()
+		daBack := SliceArrayFromBytes(daBytes)
+		require.EqualValues(t, da.NumElements(), daBack.NumElements())
+		for i := 0; i < 500; i++ {
 			require.EqualValues(t, da.At(i), daBack.At(i))
 		}
 	})
--- transaction.go
+++ transaction.go
@@ -1,49 +1,6 @@
 package easyutxo
 
-import "io"
+const TransactionIDLength = 32
 
 type Transaction struct {
-	Essence     TransactionEssence
-	UnlockBlock UnlockBlocks
-}
-
-type TransactionEssence struct {
-	Inputs       Inputs
-	Outputs      Outputs
-	DataElements DataElements
-}
-
-type UnlockBlocks []UnlockBlock
-type UnlockBlock []byte
-
-type DataElements []DataElement
-type DataElement []byte
-
-type Inputs []Input
-type Input []byte
-
-type Outputs []Output
-
-type Output struct {
-	Assets OutputAssets
-	Blocks OutputBlocks
-}
-
-type OutputAssets []byte
-
-type OutputBlocks []OutputBlock
-
-type OutputBlock struct {
-	Invocation Invocation
-	//Script     Script
-}
-
-// Invocation
-//   0 inline, the script and bytes is right after
-//   1 script attached, next 32 or 20 bytes are hash of the script | DEAS of the script
-//   other - well known embeded scripts for
-
-type Invocation interface {
-	Read(r io.Reader) error
-	Write(w io.Writer) error
 }
